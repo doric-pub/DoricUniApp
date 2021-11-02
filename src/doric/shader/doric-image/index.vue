eafbdf0d24d14efaf66e10d299189ef510d1e963
王劲鹏 <jingpeng.wang@outlook.com>
2021-11-02 17:38:38 +0800

    add mode & blur

--- src/doric/shader/doric-image/index.vue
+++ src/doric/shader/doric-image/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <image class="doric-image" :src="imageUrl" :style="cssStyle" @load="onload">
+  <image
+    class="doric-image"
+    :src="imageUrl"
+    :style="cssStyle"
+    @load="onload"
+    :mode="mode"
+  >
   </image>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-import { Image } from "doric";
+import { Image, ScaleType } from "doric";
 import { DoricModel, toCSSStyle } from "../../../doric/utils";
 
 export default Vue.extend({
@@ -21,19 +27,36 @@ export default Vue.extend({
       handler(newVal) {
         const props = (newVal as DoricModel).nativeViewModel
           .props as Partial<Image>;
-        this.$set(
-          this.$data,
-          "cssStyle",
-          toCSSStyle((newVal as DoricModel).cssStyle)
-        );
 
         if (props.imageUrl) {
           this.$set(this.$data, "imageUrl", props.imageUrl);
         }
-        
+
         if (props.imageBase64) {
           this.$set(this.$data, "imageUrl", props.imageBase64);
         }
+
+        if (props.scaleType) {
+          switch (props.scaleType) {
+            case ScaleType.ScaleAspectFit:
+              this.$set(this.$data, "mode", "aspectFit");
+              break;
+            case ScaleType.ScaleAspectFill:
+              this.$set(this.$data, "mode", "aspectFill");
+              break;
+            default:
+              this.$set(this.$data, "mode", "scaleToFill");
+              break;
+          }
+        }
+
+        let cssStyle = (newVal as DoricModel).cssStyle;
+
+        if (props.isBlur) {
+          cssStyle["filter"] = "blur(8px)";
+        }
+
+        this.$set(this.$data, "cssStyle", toCSSStyle(cssStyle));
       },
     },
   },
@@ -41,6 +64,7 @@ export default Vue.extend({
     return {
       cssStyle: null,
       imageUrl: null,
+      mode: "scaleToFill",
     };
   },
 
